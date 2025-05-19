29bc961c8fb6ae8e16f3456f161006e967f3db60
ParkJWoo <character453@naver.com>
2025-05-19 12:16:41 +0900

    feat: 체력바 UI 구현
    1. 체력바, 스태미너 UI 바 구현 완료
    
    2. 현재 게임 시작 시, 시간에 따라 체력이 지속적으로 깎이게금 구현됨 → UI 상태 변화 확인용

--- SpartaDungeonExplore/Assets/1. Scripts/1. Player/Player.cs
+++ SpartaDungeonExplore/Assets/1. Scripts/1. Player/Player.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public PlayerController playerController;
+    public PlayerCondition playerCondition;
 
     private void Awake()
     {
         CharacterManager.Instance.Player = this;
+
         playerController = GetComponent<PlayerController>();
+        playerCondition = GetComponent<PlayerCondition>();
     }
 }
---
+++ SpartaDungeonExplore/Assets/1. Scripts/1. Player/PlayerCondition.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCondition : MonoBehaviour
+{
+    public UICondition uiCondition;
+
+    Condition health { get { return uiCondition.health; } }
+    Condition stamina { get { return uiCondition.stamina; } }
+
+    public event Action onTakeDamage;
+
+    private void Update()
+    {
+        health.Subtract(health.passiveValue * Time.deltaTime);
+
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if(health.curValue < 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+
+    public void Die()
+    {
+        Debug.Log("[PlayerCondition] 플레이어 사망");
+    }
+}
